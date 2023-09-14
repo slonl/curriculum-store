a6a1ed97048a010cdaefadaaa55690fbf57d463c
Auke van Slooten <auke@muze.nl>
2023-09-14 15:41:12 +0200

    removed /status/ endpoint for now, needs a different approach

--- src/curriculum-store.mjs
+++ src/curriculum-store.mjs
@@ -9,6 +9,22 @@ const __dirname = path.dirname(__filename);
 const datafile = process.env.DATAFILE || 'data/curriculum.jsontag'
 const port     = process.env.NODE_PORT || 3000
 
+/*simplystore.get('/status/', (req,res) => {
+	const query = `
+Object.keys(data)
+	`
+	simplystore.queryWorkerpool
+	.run({pointer:'/',{},query})
+	.then(keys => {
+		// add total memory usage to response
+		const memory = 0
+		simplystore.sendResponse({
+			keys,
+			memory
+		},res)
+	})
+})*/
+
 simplystore.run({
 	datafile: datafile,
 	port: port,
