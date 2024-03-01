b9267fb353cbe2f8cfc66f6c3a4364e4966fcbad
Auke van Slooten <auke@muze.nl>
2024-03-01 09:56:35 +0100

    removed debug log

--- src/curriculum-store.mjs
+++ src/curriculum-store.mjs
@@ -4,11 +4,6 @@ const datafile     = process.env.DATA_FILE || './data/data.jsontag'
 const commandsFile = process.env.COMMANDS || './src/commands.mjs'
 const port         = process.env.NODE_PORT || 3000
 
-import(commandsFile).then(module => {
-	console.log('Commands:',commandsFile,Object.keys(module.default))
-	console.log('Data file:',datafile)
-})
-
 simplystore.run({
 	datafile,
 	port,
