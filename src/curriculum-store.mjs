3fcdae74fd8fff2e8858949c2c182b7dacdd1de1
govert@muze.nl <govert@muze.nl>
2024-09-03 10:37:04 +0200

    the dot in './src/commands.mjs' caused the save to fail because the URL is wrong:  code: 'ERR_MODULE_NOT_FOUND', curriculum-store  |   url: 'file:///home/node/app./src/commands.mjs'

--- src/curriculum-store.mjs
+++ src/curriculum-store.mjs
@@ -2,11 +2,12 @@ import simplystore from '@muze-nl/simplystore'
 import process from 'node:process'
 
 const datafile     = process.env.DATA_FILE || './data/data.jsontag'
-const commandsFile = process.env.COMMANDS || process.cwd()+'./src/commands.mjs'
+const commandsFile = process.env.COMMANDS || process.cwd()+'/src/commands.mjs' // @TODO: maybe commit the removal of the .
 const port         = process.env.NODE_PORT || 3000
 
 simplystore.run({
 	datafile,
 	port,
 	commandsFile,
+	maxWorkers: 2
 })
