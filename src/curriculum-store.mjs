3ff383bd35f95e82eb2934568ec0b655afe1cb55
Auke van Slooten <auke@muze.nl>
2024-09-23 16:25:48 +0200

    added schemaFile

--- src/curriculum-store.mjs
+++ src/curriculum-store.mjs
@@ -2,11 +2,13 @@ import simplystore from '@muze-nl/simplystore'
 import process from 'node:process'
 
 const datafile     = process.env.DATA_FILE || './data/data.jsontag'
+const schemaFile   = process.env.SCHEMA_FILE || './data/schema.jsontag'
 const commandsFile = process.env.COMMANDS || process.cwd()+'/src/commands.mjs'
 const port         = process.env.NODE_PORT || 3000
 
 simplystore.run({
 	datafile,
+	schemaFile,
 	port,
 	commandsFile,
 })
