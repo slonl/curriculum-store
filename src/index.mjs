d3b38843f6f1ae461f8764585cbfe3a307457ca3
Auke van Slooten <auke@muze.nl>
2026-06-11 16:35:35 +0200

    added custom index file

---
+++ src/index.mjs
@@ -0,0 +1,21 @@
+import index from '@muze-nl/simplystore/src/index.mjs'
+import parentIndex from './index.parents.mjs'
+import niveauIndex from './index.niveau.mjs'
+import rootIndex from './index.roots.mjs'
+
+export default {
+	create(data, meta) {
+		index.create(data, meta)
+		parentIndex.create(data, meta)
+		rootIndex.create(data, meta)
+		niveauIndex.create(data, meta)
+	},
+	update(data, meta, changes) {
+		index.update(data, meta, changes)
+		parentIndex.update(data, meta, changes)
+		rootIndex.update(data, meta, changes)
+	},
+	load(meta, uuid=null) {
+		return index.load(meta, uuid)
+	}
+}
